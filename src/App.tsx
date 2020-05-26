c5868eb32c7637ab66ae7a9fc2471456d12a159d
Matt Hwang <hw4ng_m@yahoo.com>
2020-05-25 20:41:04 -0700

    Add typescript, react-router, material-ui

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Nav from './components/Nav';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
